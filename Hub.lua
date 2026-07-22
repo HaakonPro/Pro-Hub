fe4859ca0a0701b79234ffe53bcd29871ac891a0
HaakonPro <haakon097@gmail.com>
2026-07-22 19:40:55 +0200

    Create Hub.lua

---
+++ Hub.lua
@@ -0,0 +1,33 @@
+-- ===== Hub.lua =====
+local Hub = {}
+
+function Hub.CreateWindow()
+    local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+    local Window = Rayfield:CreateWindow({
+        Name = "H-Hub",
+        Icon = 0,
+        LoadingTitle = "H-Hub",
+        LoadingSubtitle = "by HaakonPro",
+        ShowText = "Rayfield",
+        Theme = "Default",
+        ToggleUIKeybind = "K",
+        DisableRayfieldPrompts = false,
+        DisableBuildWarnings = false,
+        ConfigurationSaving = {
+            Enabled = true,
+            FolderName = "HHub",
+            FileName = "Config"
+        },
+        Discord = {
+            Enabled = false,
+            Invite = "noinvitelink",
+            RememberJoins = true
+        },
+        KeySystem = false,
+    })
+
+    return Window, Rayfield
+end
+
+return Hub
