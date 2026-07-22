c18b0b1906930c9e9b6f6a988f21534251abdbe8
Haakon <haakon097@email.com>
2026-07-22 20:27:53 +0200

    Update

--- games/MoneyClickerInc.lua
+++ games/MoneyClickerInc.lua
@@ -2,47 +2,43 @@
 local MoneyClickerInc = {}
 
 function MoneyClickerInc.Init(Window, Rayfield, IsActiveSession)
-    local FarmTab = Window:CreateTab("Farm")
-    local Upgrades = Window:CreateTab("Upgrades")
-    local Misc = Window:CreateTab("Misc")
-    local Info = Window:CreateTab("Info")
+	local FarmTab = Window:CreateTab("Farm")
+	local Upgrades = Window:CreateTab("Upgrades")
+	local Misc = Window:CreateTab("Misc")
+	local Info = Window:CreateTab("Info")
 
-    FarmTab:CreateToggle({
-        Name = "Autoclick Money",
-        CurrentValue = false,
-        Flag = "AutoClickMoney",
-        Callback = function(Value)
-            if Value then
-                task.spawn(function()
-                    while Rayfield.Flags["AutoClickMoney"].CurrentValue and IsActiveSession() do
-                        local ok, err = pcall(function()
-                            game:GetService("ReplicatedStorage").Events.ClickMoney:FireServer()
-                        end)
-                        if not ok then
-                            warn("[MoneyClickerInc] ClickMoney failed: " .. tostring(err))
-                        end
-                        task.wait(1)
-                    end
-                end)
-            end
-        end,
-    })
+	FarmTab:CreateToggle({
+		Name = "Autoclick Money",
+		CurrentValue = false,
+		Flag = "AutoClickMoney",
+		Callback = function(Value)
+			if Value then
+				task.spawn(function()
+					while Rayfield.Flags["AutoClickMoney"].CurrentValue and IsActiveSession() do
+						game:GetService("ReplicatedStorage").Events.ClickMoney:FireServer()
+						task.wait(0.1)
+						print("ran")
+					end
+				end)
+			end
+		end,
+	})
 
-    Misc:CreateSlider({
-        Name = "WalkSpeed",
-        Range = {0, 250},
-        Increment = 5,
-        Suffix = "WalkSpeed",
-        CurrentValue = 16,
-        Flag = "PlayerWalkSpeed",
-        Callback = function(Value)
-           local character = game.Players.LocalPlayer.Character
-           local humanoid = character and character:FindFirstChild("Humanoid")
-           if humanoid then
-              humanoid.WalkSpeed = Value 
-           end
-        end,
-    })
+	Misc:CreateSlider({
+		Name = "WalkSpeed",
+		Range = { 0, 250 },
+		Increment = 5,
+		Suffix = "WalkSpeed",
+		CurrentValue = 16,
+		Flag = "PlayerWalkSpeed",
+		Callback = function(Value)
+			local character = game.Players.LocalPlayer.Character
+			local humanoid = character and character:FindFirstChild("Humanoid")
+			if humanoid then
+				humanoid.WalkSpeed = Value
+			end
+		end,
+	})
 end
 
 return MoneyClickerInc
